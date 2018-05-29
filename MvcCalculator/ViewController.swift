b517c8413b8ad439be8f8013804ce6433735a784
MomizIO <aziz@momiz.co git config --global user.name MomizIO>
2018-05-29 23:46:26 +0200

    Initial Commit

---
+++ MvcCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MvcCalculator
+//
+//  Created by Abdelaziz on 5/29/18.
+//  Copyright © 2018 ABD EL AZIZ MOHAMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
